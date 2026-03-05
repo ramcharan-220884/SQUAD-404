813f261f67cf8891a97940ee34a035f83e48bde4
ramcharan-220884 <n220884@rguktn.ac.in>
2026-03-05 16:53:58 +0530

    Initial React frontend setup for Placement Management System

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Your profile details will appear here</p>
+    </div>
+  );
+}
+
+export default Profile;
